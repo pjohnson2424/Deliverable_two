4a08b817627f1491dc156dafac3307e3a4f2ea37
Patrick Johnson <pjohnson24@icloud.com>
2020-03-18 19:52:29 -0400

    final update

--- js/script.js
+++ js/script.js
@@ -1,34 +1,33 @@
 var vacationType = prompt("What kind of trip would you like to go on? (Choose from Musical, Tropical, or Adventurous)");
    var travelSuggestion = "";
 
-   if( vacationType == "Musical" ){
-      travelSuggestion ="New Orleans";
+   if( vacationType == "musical" ){
+      travelSuggestion ="see a musical in New Orleans";
    }
 
-   else if( vacationType == "Tropical" ){
-      travelSuggestion ="Beach Vacation in Mexico";
+   else if( vacationType == "tropical" ){
+      travelSuggestion ="on a beach vacation in Mexico";
    }
 
-   else if ( vacationType == "Adventurous" ) {
-      travelSuggestion ="Whitewater Rafting the Grand Canyon";
+   else if ( vacationType == "adventurous" ) {
+      travelSuggestion ="whitewater rafting the Grand Canyon";
    }
 
-   var groupSize = prompt("How many are in your group?");
-      travelSuggestion +="";
+travelSuggestion +=" by ";
 
-      if( groupSize >= 1 && groupSize <=2 ){
-          travelSuggestion += "First Class flight";
-      }
+var groupSize = prompt("How many are in your group?");
 
-      else if( groupSize >= 3 && groupSize <= 5 ){
-         travelSuggestion +="Helicopter";
-      }
+   if( groupSize >= 1 && groupSize <=2 ){
+      travelSuggestion += "First Class flight";
+   }
 
-      else if ( groupSize >= 6 ) {
-         travelSuggestion +="Charter Flight";
-      }
+  else if( groupSize >= 3 && groupSize <= 5 ){
+      travelSuggestion +="Helicopter";
+   }
+
+  else if ( groupSize >= 6 ) {
+     travelSuggestion +="Charter Flight";
+   }
 
 
-      var result = "Since you're a group of " + groupSize + " going on a "
-      + vacationType + " vacation, you should take a " + travelSuggestion ;
-
+var result = "Since you're a group of " + groupSize + " and want to go on a " + vacationType + " vacation, you should go " + travelSuggestion;
